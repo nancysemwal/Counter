6ba6d44366e7d7c479ada99cd8993ebde6fdab9b
nancy <nancy64semwal@gmail.com>
2022-03-08 15:44:37 +0530

    Service Integration

--- app/src/main/java/com/example/counter/DroneService.kt
+++ app/src/main/java/com/example/counter/DroneService.kt
@@ -1,13 +1,88 @@
 package com.example.counter
 
+import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.MutableLiveData
+import kotlin.random.Random
 
 class DroneService : Service() {
+    private val usbReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("HAPTORK", "onReceive()")
+            if (intent != null) {
+                if(ACTION_USB_PERMISSION == intent.action) {
+                    synchronized(this) {
+                        val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                            device?.apply {
+                                Log.d("HAPTORK", "Permission Granted")
+                            }
+                        } else {
+                            Log.d("HAPTORK", "Permission is not Granted")
+                        }
+                    }
+                }
+                else if(ACTION_USB_ATTACHED == intent.action) {
+                    if(!serialPortConnected) {
+                        findSerialPortDevice()
+                    }
+                }
+            }
+        }
+    }
 
     private val binder = DroneBinder()
+
+    private val mGenerator = Random(10)
+
+    private val usbManager by lazy {
+        getSystemService(Context.USB_SERVICE) as UsbManager
+    }
+
+    private val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+    private val ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED"
+    private val serialPortConnected = false
+    var SERVICE_CONNECTED = MutableLiveData("False")
+
+    val randomNumber : Int
+        get() = mGenerator.nextInt()
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("HAPTORK","Service's onCreate()")
+        SERVICE_CONNECTED.value = "true"
+        setFilter()
+        findSerialPortDevice()
+    }
+
+    private fun setFilter() {
+        var filter = IntentFilter()
+        filter.addAction(ACTION_USB_PERMISSION)
+        filter.addAction(ACTION_USB_ATTACHED)
+        registerReceiver(usbReceiver, filter)
+    }
+
+    private fun findSerialPortDevice() {
+        Log.d("HAPTORK", "findSerialPortDevice")
+        val deviceList : HashMap<String, UsbDevice> = usbManager.deviceList
+        val intent = Intent(ACTION_USB_PERMISSION)
+        deviceList.values.forEach(){
+            val pendingIntent = PendingIntent.getBroadcast(this, 0, intent
+            , 0)
+            usbManager.requestPermission(it, pendingIntent)
+        }
+    }
+
     inner class DroneBinder : Binder(){
         fun getService() : DroneService = this@DroneService
     }
@@ -15,4 +90,9 @@ class DroneService : Service() {
     override fun onBind(intent: Intent): IBinder {
         return binder
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_NOT_STICKY
+    }
+
 }
--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -18,6 +19,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
@@ -34,6 +36,7 @@ class MainActivity : ComponentActivity() {
     private var bound : Boolean = false
     private val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.d("HAPTORK", "Service is Connected")
             val binder = service as DroneService.DroneBinder
             droneService = binder.getService()
             bound = true
@@ -45,11 +48,13 @@ class MainActivity : ComponentActivity() {
 
     }
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("HAPTORK", "HELLO")
         counterViewModel.updateCount()
         super.onCreate(savedInstanceState)
         setContent {
             CounterTheme {
-                MainScreen(counterViewModel = counterViewModel)
+                //MainScreen(counterViewModel = counterViewModel)
+                MainScreen(counterViewModel = counterViewModel, droneService = droneService)
             }
         }
         //counterViewModel.updateCount()
@@ -60,6 +65,7 @@ class MainActivity : ComponentActivity() {
         Intent(this, DroneService::class.java).also { intent ->
             bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
         }
+        Log.d("HAPTORK", "onStart")
     }
 
     override fun onStop() {
@@ -67,13 +73,23 @@ class MainActivity : ComponentActivity() {
         unbindService(serviceConnection)
         bound = false
     }
+
+    fun onClick(){
+        if(bound) {
+            val num : Int = droneService.randomNumber
+            Toast.makeText(this, "Number : $num", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
 
 @Composable
-fun MainScreen(counterViewModel: CounterViewModel){
+fun MainScreen(counterViewModel: CounterViewModel,
+            droneService: DroneService){
     val count by counterViewModel.counter.observeAsState()
+    val connected by droneService.SERVICE_CONNECTED.observeAsState()
     Column() {
         Text(text = count.toString())
+        Text(text = "Service Connected: " + connected.toString())
         Button(onClick = {counterViewModel.updateCount()}) {
             Text(text = "Update")
         }
@@ -89,4 +105,5 @@ class CounterViewModel : ViewModel(){
         Log.d("SEE","inside update")
         _count.value = ++count
     }
+
 }
