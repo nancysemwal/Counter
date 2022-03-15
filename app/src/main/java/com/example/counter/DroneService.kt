77c4214fb7484acc96e56ad0edfbfab34d61759c
nancy <nancy64semwal@gmail.com>
2022-03-15 17:58:57 +0530

    Made MAVLINK connection

--- app/src/main/java/com/example/counter/DroneService.kt
+++ app/src/main/java/com/example/counter/DroneService.kt
@@ -12,8 +12,16 @@ import android.hardware.usb.UsbManager
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.MutableLiveData
+import com.felhr.usbserial.SerialInputStream
+import com.felhr.usbserial.SerialOutputStream
+import com.felhr.usbserial.UsbSerialDevice
+import io.dronefleet.mavlink.MavlinkConnection
+import io.dronefleet.mavlink.MavlinkMessage
+import io.dronefleet.mavlink.common.Attitude
+import io.dronefleet.mavlink.common.RequestDataStream
+import java.io.EOFException
+import java.io.IOException
 import kotlin.random.Random
 
 class DroneService : Service() {
@@ -21,12 +29,17 @@ class DroneService : Service() {
 
     private var _setUsbStatus : (Boolean) -> Unit = { b -> {}}
     private var _setBoundStatus : (Boolean) -> Unit = { b -> {}}
+    private var _setUsbConnection : (String) -> Unit = { b -> {}}
     fun setUsbStatus(_fn: (Boolean)->Unit) {
         _setUsbStatus = _fn
     }
 
     fun setBoundStatus(_fn: (Boolean) -> Unit) {
         _setBoundStatus = _fn
+    }
+
+    fun setUsbConnection(_fn: (String) -> Unit){
+        _setUsbConnection = _fn
     }
 
     private val usbReceiver = object : BroadcastReceiver(){
@@ -38,11 +51,14 @@ class DroneService : Service() {
                         val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
                         if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                             device?.apply {
-                                Log.d("HAPTORK", "Permission Granted")
+                                Log.d("Drishi", "Permission Granted")
                                 usbConnected.value = true
                                 usbConnected2 = true
                                 _setUsbStatus(true)
-                                //connection = usbManager.openDevice(device)
+                                usbConnection = usbManager.openDevice(device)
+                                usbDevice = device
+                                Log.d("Drishi", "$usbConnection")
+                                startMav()
                             }
                         } else {
                             Log.d("HAPTORK", "Permission is not Granted")
@@ -69,7 +85,10 @@ class DroneService : Service() {
     private val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
     private val ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED"
     private val serialPortConnected = false
-    private lateinit var connection : UsbDeviceConnection
+    private lateinit var usbConnection : UsbDeviceConnection
+    private lateinit var usbDevice: UsbDevice
+    private lateinit var serialPort : UsbSerialDevice
+    private lateinit var mavlinkConnection : MavlinkConnection
     var usbConnected = MutableLiveData<Boolean>(false)
     var usbConnected2 = false
     var SERVICE_CONNECTED = MutableLiveData<Boolean>(false)
@@ -100,6 +119,52 @@ class DroneService : Service() {
             val pendingIntent = PendingIntent.getBroadcast(this, 0, intent
             , 0)
             usbManager.requestPermission(it, pendingIntent)
+        }
+    }
+
+    private fun startMav(){
+        if(usbDevice != null && usbConnection != null){
+            serialPort = UsbSerialDevice.createUsbSerialDevice(usbDevice, usbConnection)
+            Log.d("drishi", "$serialPort")
+        }
+        if(serialPort != null){
+            if(serialPort.syncOpen()){
+                serialPort.setBaudRate(57600)
+                var inputStream : SerialInputStream = serialPort.inputStream
+                //Log.d("strm",inputStream.read().toString())
+                var outputStream : SerialOutputStream = serialPort.outputStream
+                mavlinkConnection = MavlinkConnection.create(inputStream, outputStream)
+                Log.d("Drishi", "$mavlinkConnection")
+                var requestDataStreamMsg : RequestDataStream = RequestDataStream.builder()
+                    .targetSystem(1)
+                    .targetComponent(0)
+                    .reqStreamId(0)
+                    .reqMessageRate(1)
+                    .startStop(1)
+                    .build()
+                try {
+                    mavlinkConnection.send2(255, 0, requestDataStreamMsg)
+                } catch (e : IOException){
+                    Log.d("xxception",e.stackTraceToString())
+                }
+                Log.d("mavlink", mavlinkConnection.toString())
+                try {
+                    var mavMsg = mavlinkConnection.next()
+                }
+                catch (e : EOFException){
+                    Log.d("xxception2", e.stackTraceToString())
+                }
+
+//                while(mavlinkConnection.next() != null){
+//                    mavMsg = mavlinkConnection.next()
+//                    if(mavMsg.payload is Attitude){
+//                        Log.d("attitude", "${mavMsg.payload}")
+//                    }else{
+//                        Log.d("attitude", "hello")
+//                    }
+//                }
+                //Log.d("Mavlink", "${mavMsg.payload.toString()}")
+            }
         }
     }
 
