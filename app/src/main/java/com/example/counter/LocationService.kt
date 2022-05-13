e50a86132ec91197abe96f3bf38be96550adcb1a
nancy <nancy64semwal@gmail.com>
2022-05-13 16:52:18 +0530

    removed getDirection() method

--- app/src/main/java/com/example/counter/LocationService.kt
+++ app/src/main/java/com/example/counter/LocationService.kt
@@ -185,32 +185,8 @@ class LocationService : Service(), LocationListener, SensorEventListener {
 
         val angle = round(degrees * 100) / 100
 
-        val direction = getDirection(degrees)
         _setYawSensor(angle)
         return angle
-    }
-
-    private fun getDirection(angle: Double): String {
-        var direction = ""
-
-        if (angle >= 350 || angle <= 10)
-            direction = "N"
-        if (angle < 350 && angle > 280)
-            direction = "NW"
-        if (angle <= 280 && angle > 260)
-            direction = "W"
-        if (angle <= 260 && angle > 190)
-            direction = "SW"
-        if (angle <= 190 && angle > 170)
-            direction = "S"
-        if (angle <= 170 && angle > 100)
-            direction = "SE"
-        if (angle <= 100 && angle > 80)
-            direction = "E"
-        if (angle <= 80 && angle > 10)
-            direction = "NE"
-
-        return direction
     }
 
 }
